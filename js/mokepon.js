05c1deed1f9fe00ea1bc67c42d58eabb57e64cb5
Jose Luis Pauluk <josepauluk@gmail.com>
2022-08-02 19:47:15 -0300

    finalizada clase 22 culmindado seleccion de personajes propios y del enemigo

--- js/mokepon.js
+++ js/mokepon.js
@@ -4,7 +4,37 @@ function iniciarJuego() {
 }
 
 function seleccionarMascotaJugador() {
-  alert("seleccionaste tu mascota");
+  let inputHipodoge = document.getElementById("hipodoge");
+  let inputCapipepo = document.getElementById("capipepo");
+  let inputRatigueya = document.getElementById("ratigueya");
+  let spanMascotaJugador = document.getElementById("mascota-jugador");
+
+  if (inputHipodoge.checked) {
+    spanMascotaJugador.innerHTML = "Hipodoge";
+  } else if (inputCapipepo.checked) {
+    spanMascotaJugador.innerHTML = "Capipepo";
+  } else if (inputRatigueya.checked) {
+    spanMascotaJugador.innerHTML = "Ratigueya";
+  } else {
+    alert("Selecciona una mascota");
+  }
+  seleccionarMascotaEnemigo();
 }
 
+function seleccionarMascotaEnemigo() {
+  let ataqueAleatorio = aleatorio(1, 3);
+  let spanMascotaEnemigo = document.getElementById("mascota-enemigo");
+
+  if (ataqueAleatorio == 1) {
+    spanMascotaEnemigo.innerHTML = "Hipodogue";
+  } else if (ataqueAleatorio == 2) {
+    spanMascotaEnemigo.innerHTML = "Capipepo";
+  } else {
+    spanMascotaEnemigo.innerHTML = "Ratigueya";
+  }
+}
+
+function aleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 window.addEventListener("load", iniciarJuego);
